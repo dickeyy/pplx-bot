ecffa6fe3a49e904da42302cf71a3a157d5e2f59
Kyle Dickey <k@kyle.so>
2025-01-16 20:12:01 -0700

    add custom status

--- events/ready.go
+++ events/ready.go
@@ -10,5 +10,6 @@ func init() {
 }
 
 func onReady(s *discordgo.Session, r *discordgo.Ready) {
+	s.UpdateCustomStatus("What do you want to know?")
 	log.Info().Msgf("Signed in as %s", s.State.User.String())
 }
